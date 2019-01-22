f5f11199f5e74175b3ea334264caa262575c9218
Aaiza Khan <36559905+AaizaKhan@users.noreply.github.com>
2019-01-22 13:52:45 +0500

    Add files via upload

---
+++ task8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> <?php 
+		echo "$var";
+		 ?> 	
+	</title>
+</head>
+<body>
+	<?php 
+		$var= 'PHP LAB6'
+		 ?>
+	<H3> <?php 
+		echo "$var";
+		 ?> 	
+	 </H3>
+	<a href="https://www.w3schools.com">
+		<?php 
+		echo "$var";
+		 ?> 	
+	</a>
+
+</body>
+</html>
---
+++ task9.php
@@ -0,0 +1,23 @@
+<?php 
+	
+	   echo           "<table border=\"1\" cellpadding=\"15\" cellspacing=\"0\">";
+					echo				"<tr>";
+					echo					"<td> Salary of MR.A is </td>";
+					echo					"<td> 1000$ </td>";
+					echo				"</tr>";
+
+					echo				"<tr>";
+					echo					"<td> Salary of MR.B is </td>";
+					echo					"<td> 1200$ </td>";
+					echo				"</tr>";
+
+
+					echo				"<tr>";
+					echo					"<td> Salary of MR.C is</td>";
+					echo					"<td> 1400$</td>";
+					echo				"</tr>";
+	echo        	"</table>";
+
+
+
+ ?>
